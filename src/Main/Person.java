736c80cb456294120e8f9958aec5489a925dcf36
EricRinger6 <eric.ringer6@gmail.com>
2020-12-03 21:17:27 -0500

    master

---
+++ src/Main/Person.java
@@ -0,0 +1,74 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+public class Person implements CheckPerson{
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        LocalDate now = LocalDate.now();
+        Period difference = Period.between(birthday, now);
+        return difference.getYears();
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    @Override
+    public boolean test(Person p) {
+        return false;
+    }
+
+    public void printPerson() {
+        String output = "Name: "+this.name+" Birthdate: "+this.birthday+" Gender: "+this.gender+" Email: "+emailAddress;
+        System.out.println(output);
+    }
+
+    public static void printPersonsWithinAgeRange(List<Person> roster, int low, int high) {
+        for (Person p : roster) {
+            if (low <= p.getAge() && p.getAge() < high) {
+                p.printPerson();
+            }
+        }
+    }
+
+    public static void printPersons(List<Person> roster, CheckPerson tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                p.printPerson();
+            }
+        }
+    }
+
+    public static void printPersonsOlderThan(List<Person> roster, int age) {
+        for (Person p : roster) {
+            if (p.getAge() >= age) {
+                p.printPerson();
+            }
+        }
+    }
+}
